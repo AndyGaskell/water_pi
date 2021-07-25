b528627d52b0ef032d33a8af9c0cab6006ec61e0
Andy Gaskell <ag@ssofb.co.uk>
2021-07-25 11:49:13 +0100

    sensor test script

---
+++ src/test_one_sensor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this is a test script for the sensor 
+# it just reads a sensor value
+
+sensor_gpio=21
+echo "sensor gpio: $sensor_gpio "
+
+gpio -g mode $sensor_gpio in
+echo "sensor set-up"
+sleep 2
+
+# check the sensor
+sensor_value = $(gpio read $sensor_gpio)
+echo "sensor value: $sensor_value "
+
+sleep 2
+
+# check the sensor
+sensor_value = $(gpio read $sensor_gpio)
+echo "sensor value: $sensor_value "
+
+sleep 2
+
+# check the sensor
+sensor_value = $(gpio read $sensor_gpio)
+echo "sensor value: $sensor_value "
