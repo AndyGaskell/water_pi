897417dbb4151fe18cfab8d19e8a3e2cc5082eb4
Andy Gaskell <ag@ssofb.co.uk>
2021-07-25 18:21:04 +0100

    python version

---
+++ src/test_one_sensor.py
@@ -0,0 +1,29 @@
+# this is a test script for the sensor 
+# it just reads a sensor value
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+DIGIOUT = 21
+GPIO.setup(DIGIOUT, GPIO.IN)
+time.sleep(2)
+
+sensor_value = GPIO.input(DIGIOUT)
+print('sensor value: ', sensor_value)
+
+time.sleep(2)
+
+sensor_value = GPIO.input(DIGIOUT)
+print('sensor value: ', sensor_value)
+
+time.sleep(2)
+
+sensor_value = GPIO.input(DIGIOUT)
+print('sensor value: ', sensor_value)
+
+time.sleep(2)
+
+GPIO.cleanup()
