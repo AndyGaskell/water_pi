b12c41ba6519a80c5a95fe13c5a21d7e4fea7c8b
Andy Gaskell <ag@ssofb.co.uk>
2021-07-28 22:56:04 +0100

    Update water_cycle.sh

--- src/water_cycle.sh
+++ src/water_cycle.sh
@@ -50,24 +50,36 @@ echo "set-up"
 gpio -g mode 27 out
 gpio -g mode 24 out
 gpio -g mode $motor_1_gpio out
+sleep 1
+gpio -g write 27 1
+gpio -g write 24 0
 echo "motor1 set"	
 
 # set-up motor 2
 gpio -g mode 22 out
 gpio -g mode 6 out
 gpio -g mode $motor_2_gpio out
+sleep 1
+gpio -g write 22 1
+gpio -g write 6 0
 echo "motor2 set"	
 
 # set-up motor 3
 gpio -g mode 23 out
 gpio -g mode 16 out
 gpio -g mode $motor_3_gpio out
+sleep 1
+gpio -g write 23 1
+gpio -g write 16 0
 echo "motor3 set"	
 
 # set-up motor 4
 gpio -g mode 13 out
 gpio -g mode 18 out
 gpio -g mode $motor_4_gpio out
+sleep 1
+gpio -g write 13 1
+gpio -g write 18 0
 echo "motor4 set"	
 
 # set up sensors
